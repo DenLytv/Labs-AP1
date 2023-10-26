7f43349550fdd6068de7b4f7bf15551849fe6e32
Den <widny6114@gmail.com>
2023-10-26 21:30:14 +0300

    Lab 4 Creating

---
+++ Lab4/Task1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#define N 12
+
+void fillArr(std::vector<int>& arr);
+void calculation(std::vector<int> arr, int& maxOdd, int& evenCount);
+
+int main()
+{
+	srand(static_cast<unsigned>(time(NULL)));
+	std::vector<int> arr(N);
+	int maxOdd = 2, evenCount = 0;
+
+	fillArr(arr);
+	calculation(arr, maxOdd, evenCount);
+
+	if (maxOdd == 2)
+		std::cout << "There are no odd numbers in array" << std::endl;
+	else
+		std::cout << "Max odd number: " << maxOdd << std::endl;
+
+	std::cout << "Count of even: " << evenCount << std::endl;
+
+	return 0;
+}
+
+void fillArr(std::vector<int>& arr)
+{
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = rand() % 201 - 100;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void calculation(std::vector<int> arr, int& maxOdd, int& evenCount)
+{
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[i] % 2 == 0)
+			++evenCount;
+		else if (arr[i] % 2 != 0 && arr[i] > maxOdd)
+			maxOdd = arr[i];
+	}
+}
---
+++ Lab4/Task2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+#include <string>
+#define N 4
+
+int** reverseColumns(int** arr);
+int files(int**& arr, std::string num);
+
+int main()
+{
+    srand(static_cast<unsigned>(time(NULL)));
+    int** ptr = new int* [N];
+    for (int i = 0; i < N; i++)
+    {
+        ptr[i] = new int[N];
+    }
+
+    if (files(ptr, "1") == 1) return 1;
+    ptr = reverseColumns(ptr);
+    if (files(ptr, "2") == 1) return 1;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        delete[] ptr[i];
+    }
+
+    delete[] ptr;
+
+    return 0;
+}
+
+int** reverseColumns(int** arr)
+{
+    int** temp = new int* [N];
+    int tmp;
+    for (int i = 0; i < N; i++)
+    {
+        temp[i] = new int[N];
+        for (int j = 0; j < N; j++)
+        {
+            tmp = arr[i][j];
+            temp[i][j] = arr[i][N - (j + 1)];
+            temp[i][N - (j + 1)] = tmp;
+        }
+    }
+    return temp;
+}
+
+int files(int**& arr, std::string num)
+{
+    std::ofstream F_w("..\\F" + num + ".txt");
+    if (!F_w) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (num == "1")
+                arr[i][j] = rand() % 201 - 100;
+            F_w << std::setw(8) << arr[i][j];
+        }
+        F_w << std::endl;
+    }
+    F_w.close();
+
+    std::ifstream F_r("..\\F" + num + ".txt");
+    if (!F_r) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            F_r >> arr[i][j];
+            std::cout << std::setw(8) << arr[i][j];
+        }
+        std::cout << std::endl;
+    }
+    F_r.close();
+    std::cout << std::endl;
+
+    return 0;
+}
