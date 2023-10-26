b30adbfb49fa9a5ce97c286cc7d2965b63ebf9e7
Den <widny6114@gmail.com>
2023-10-26 21:32:26 +0300

    Lab 5 Creating

---
+++ Lab5/Task1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+
+std::pair <std::string, size_t> task(std::string myname);
+std::string input();
+bool check(std::string fullName);
+
+int main() {
+
+    std::string fullName = input();
+
+    std::cout << "Name: " << task(fullName).first << std::endl;
+    std::cout << "Number of letters in third word : " << task(fullName).second << std::endl;
+
+    return 0;
+}
+
+std::pair <std::string, size_t> task(std::string fullName) {
+    int found1 = fullName.find_first_of(" ") + 1;
+    int found2 = fullName.find_last_of(" ");
+
+    int lettersCounter = 0;
+    for (size_t i = found2 + 1, l = fullName.length(); i < l; i++)
+    {
+        if (isalpha(fullName[i]))
+            ++lettersCounter;
+    }
+
+    return { fullName.substr(found1, found2 - found1), lettersCounter };
+}
+
+std::string input() {
+    std::string fullName;
+    do
+    {
+        std::cout << "Please enter your full name(ex. Lytvynenko Denys Olexandrovych): ";
+        getline(std::cin, fullName);
+    } while (!check(fullName));
+    return fullName;
+}
+
+bool check(std::string fullName) {
+    int spacesCounter = 0;
+    for (size_t i = 0, l = fullName.length(); i < l; i++)
+    {
+        if (isspace(fullName[i]))
+            ++spacesCounter;
+    }
+    if (spacesCounter == 2)
+    {
+        int found1 = fullName.find_first_of(" ") + 1;
+        int found2 = fullName.find_last_of(" ") + 1;
+        if (isupper(fullName[0]) && isupper(fullName[found1]) && isupper(fullName[found2]))
+            return true;
+        else {
+            std::cout << "Error full name\n";
+            return false;
+        }
+    }
+    else {
+        std::cout << "Error full name\n";
+        return false;
+    }
+}
---
+++ Lab5/Task2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+int letterCount(std::string S);
+std::string input(int num);
+
+int main()
+{
+    std::string s1 = input(1);
+    std::string s2 = input(2);
+    std::cout << "The number of consonants surrounded by numbers in S1: " << letterCount(s1) << std::endl;
+    std::cout << "The number of consonants surrounded by numbers in S2: " << letterCount(s2) << std::endl;
+    return 0;
+}
+
+int letterCount(std::string s) {
+    int counter = 0;
+    for (size_t i = 1, l = s.length() - 1; i < l; i++)
+    {
+        if (isalpha(s[i]))
+            if ((tolower(s[i]) != 'a') && (tolower(s[i]) != 'e') && (tolower(s[i]) != 'y') && (tolower(s[i]) != 'u')
+                && (tolower(s[i]) != 'i') && (tolower(s[i]) != 'o'))
+                if (isdigit(s[i - 1]) && isdigit(s[i + 1]))
+                    ++counter;
+    }
+    return counter;
+}
+
+std::string input(int num) {
+    std::string s;
+    do
+    {
+        std::cout << "Please enter string " << num << " (up to 100 characters) : \n";
+        std::getline(std::cin, s);
+    } while (s.length() > 100 || s.length() == 0);
+    return s;
+}
